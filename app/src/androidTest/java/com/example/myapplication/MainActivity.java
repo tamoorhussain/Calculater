0be747f97e1e470ba92d52ac5547c68e8a02dc67
TAMOOR HUSSAIN <72344383+tamoorhussain@users.noreply.github.com>
2020-12-21 12:53:55 +0500

    Add files via upload

---
+++ app/src/androidTest/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package abhiandroid.navigationdrawerexample;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout dLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setNavigationDrawer(); // call method
+
+    }
+
+    private void setNavigationDrawer() {
+        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout); // initiate a DrawerLayout
+        NavigationView navView = (NavigationView) findViewById(R.id.navigation); // initiate a Navigation View
+        // implement setNavigationItemSelectedListener event on NavigationView
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                Fragment frag = null; // create a Fragment Object
+                int itemId = menuItem.getItemId(); // get selected menu item's id
+
+                // check selected menu item's id and replace a Fragment Accordingly
+                if (itemId == R.id.first) {
+                    frag = new FirstFragment();
+                } else if (itemId == R.id.second) {
+                    frag = new SecondFragment();
+                } else if (itemId == R.id.third) {
+                    frag = new ThirdFragment();
+                }
+                // display a toast message with menu item's title
+                Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                if (frag != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame, frag); // replace a Fragment with Frame Layout
+                    transaction.commit(); // commit the changes
+                    dLayout.closeDrawers(); // close the all open Drawer Views
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
